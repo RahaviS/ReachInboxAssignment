e7633569ae0b64498190f945eb4c2579bc9ce2bd
RahaviS <rahavikavi.sekar95@gmail.com>
2024-08-26 12:09:47 +0530

    onebox link updated

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -11,8 +11,9 @@ const Login = () => {
         <div className="login-page">
           <div className='login-contents'>
             <h1 className='page-title'>Create a new account</h1>
-            <Link to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:3000/onebox"
-            className='nav-link'>
+            {/* <Link to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:3000/onebox"
+            className='nav-link'> */}
+               <Link to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reach-inbox-assignment-ochre.vercel.app/onebox" className='nav-link'>
             <button className='custom-sign-in-btn'>
               <div className='btn-contents'><FcGoogle size={20}/> <p className='btn-text'>Sign Up with Google</p></div>
             </button></Link>
